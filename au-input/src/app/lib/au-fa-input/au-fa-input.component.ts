5b7f755ce2010e70f9c45166a5463bf01ce29678
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2022-09-14 19:25:53 +0530

    first angular library

---
+++ au-input/src/app/lib/au-fa-input/au-fa-input.component.ts
@@ -0,0 +1,46 @@
+import { AfterContentInit, Component, ContentChild, Input, OnInit, HostBinding } from '@angular/core';
+import { InputRefDirective } from '../common/input-ref.directive';
+
+@Component({
+  selector: 'au-fa-input',
+  templateUrl: './au-fa-input.component.html',
+  styleUrls: ['./au-fa-input.component.css']
+})
+export class AuFaInputComponent implements OnInit, AfterContentInit {
+
+  @Input()
+  icon: string
+
+  @ContentChild(InputRefDirective,{static:false})
+  input: InputRefDirective;
+
+  constructor() { }
+  ngAfterContentInit(): void {
+    if(!this.input) {
+      console.error('component needs an input');
+    }
+    
+  }
+
+  ngOnInit() {
+   // console.log(this.input);
+  }
+
+  @HostBinding('class.input-focus') 
+  get isInputFocus() {
+    return this.input ? this.input.focus : false;
+  }
+
+  get classes() {
+    const cssClassess = {
+      'fa':true
+    };
+
+    if(this.icon) {
+      cssClassess['fa-' + this.icon] = true;
+    }
+   // console.log(cssClassess)
+    return cssClassess;
+  }
+
+}
